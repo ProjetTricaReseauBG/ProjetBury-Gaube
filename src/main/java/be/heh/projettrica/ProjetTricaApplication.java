39f14ea1d4de4b159907346d3393c09bda5803f3
Hamnock <Nockbury@gmail.Com>
2022-11-14 08:50:49 +0100

    first commit

---
+++ src/main/java/be/heh/projettrica/ProjetTricaApplication.java
@@ -0,0 +1,13 @@
+package be.heh.projettrica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetTricaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetTricaApplication.class, args);
+    }
+
+}
