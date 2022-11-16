7bda387cebfb42df3c94dcf8ff9f55de357c8e59
Hamnock <Nockbury@gmail.Com>
2022-11-16 08:59:09 +0100

    add usecase

---
+++ src/main/java/be/heh/projettrica/domain/usecase/TournoisUsecase.java
@@ -0,0 +1,18 @@
+package be.heh.projettrica.domain.usecase;
+
+import be.heh.projettrica.domain.entities.Tournois;
+import be.heh.projettrica.domain.port.in.PortInTournois;
+import be.heh.projettrica.domain.port.out.PortOutTournois;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class TournoisUsecase implements PortInTournois {
+
+    private final PortOutTournois portouttournois;
+    @Override
+    public List<Tournois> AfficheTournois() {
+        return portouttournois.getTournoisList();
+    }
+}
