5cd03a4bf55c6d14cab8fe84f942921aa3623197
Hamnock <Nockbury@gmail.Com>
2022-11-28 14:11:05 +0100

    Debug + Test OK + Add css and html

--- src/main/java/be/heh/projettrica/ConfigurateurTournois.java
+++ src/main/java/be/heh/projettrica/ConfigurateurTournois.java
@@ -7,9 +7,12 @@ import be.heh.projettrica.infra.addaptater.secondary.TournoisMapper;
 import be.heh.projettrica.infra.addaptater.secondary.TournoisPersistenceAdapter;
 import be.heh.projettrica.infra.addaptater.secondary.orm.TournoisRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories
@@ -29,4 +32,5 @@ public class ConfigurateurTournois {
     public PortOutTournois getPortOutTournois(){
         return new TournoisPersistenceAdapter(tournoisRepository,tournoisMapper);
     }
+
 }
--- src/main/java/be/heh/projettrica/ProjetTricaApplication.java
+++ src/main/java/be/heh/projettrica/ProjetTricaApplication.java
@@ -2,6 +2,7 @@ package be.heh.projettrica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class ProjetTricaApplication {
