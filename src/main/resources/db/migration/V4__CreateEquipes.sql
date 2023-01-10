b0c6a1d666ab0d8bd21a719985c91cb0560e7e6d
Samuel <samuel.gaube@std.heh.be>
2023-01-10 14:46:55 +0100

    Modifications HTML/CSS

--- src/main/resources/db/migration/V4__CreateEquipes.sql
+++ src/main/resources/db/migration/V4__CreateEquipes.sql
@@ -1,3 +1,3 @@
-insert into equipes(nameequipes,jeux,nbplayer,visible,nbwin,idequipe) VALUES ('name1','jeux1',12,1,13,45);
+insert into equipes(nameequipes,jeux,nbplayer,visible,nbwin,idequipe) VALUES ('name1','jeux1',50,1,13,45);
 insert into equipes(nameequipes,jeux,nbplayer,visible,nbwin,idequipe) VALUES ('name2','jeux2',13,1,43,56);
 insert into equipes(nameequipes,jeux,nbplayer,visible,nbwin,idequipe) VALUES ('name3','jeux3',14,1,56,76);
